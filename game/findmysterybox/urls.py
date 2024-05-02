ab62f874b7e0366cc51f69d9ce3bffe2e9d5eaec
AlexandraFernandezG <adfernandez.17@est.ucab.edu.ve>
2024-05-02 18:15:43 -0400

    Update: fix urlspatterns

--- game/findmysterybox/urls.py
+++ game/findmysterybox/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('riddles', views.getRiddle), 
-    path('quotes', views.getQuote)
+    path('riddle', views.getRiddle), 
+    path('quote', views.getQuote)
 
 ]
