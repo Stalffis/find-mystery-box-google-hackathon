b75312c82138df0b24ad585a877ce4c8ac689b9a
Stalffis <angelmarcel.ojeda@gmail.com>
2024-05-02 21:37:18 -0400

    Add modals .js file and more
    Now the page have a .js file, modals, and some logic in the JavaScript file.

---
+++ game/findmysterybox/static/javascript.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $('#exampleModal').modal('show');
+
+    $('#modalButton').click(function(){
+        $('#exampleModal2').modal('show');
+    });
+
+    $('#modalButton2').click(function(){
+        $('#exampleModal3').modal('show');
+    });
+
+    $('#modalButton3').click(function(){
+        $('#exampleModal4').modal('show');
+    });
+});
+
+async function getRiddle() {
+    let response = await fetch('/riddle');
+    let data = await response.json();
+
+    var location = data.location
+    var state = data.state
+    var riddle = data.message
+    $('#modalText2').text(riddle);
+
+
+    $('#location').text(location);
+    $('#state').text(state);
+    
+    //console.log(data); // This will log the JSON object to your console
+}
+getRiddle();
+
+
+async function getQuote() {
+    let response = await fetch('/quote');
+    let data = await response.json();
+
+    var quote = data.message
+    $('#modalText4').text(quote);
+    
+    //console.log(data); // This will log the JSON object to your console
+}
+getQuote();
+
+
+
+
+
+//console.log("Hello, world!");
